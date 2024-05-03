ca8abd1b133a737d801597bd69fabb53ab4618f7
MarioAndreyHA <168862245+MarioAndreyHA@users.noreply.github.com>
2024-05-03 13:15:01 -0600

    Delete ProyectoConGrafica/Proyecto/Proyecto/Proyecto directory

--- ProyectoConGrafica/Proyecto/Proyecto/Proyecto/script.js
+++
@@ -1,7 +0,0 @@
-let menu = document.querySelector('#menu');
-let navlist = document.querySelector('.navlist');
-
-menu.onclick = () => {
-    menu.classList.toggle('bx-x');
-    navlist.classList.toggle('open')
-}
